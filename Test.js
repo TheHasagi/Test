bc5c2bf2d58043af1ff933a3adaefb8098b69479
Alexander <37797229+TheHasagi@users.noreply.github.com>
2019-07-30 12:07:23 +0300

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Testing extension
+// @namespace    www.testingtest.com
+// @version      1.0.0
+// @description  Testing test
+// @author       TheHasagi
+// @match        https://*.youtube.com/*
+// @compatible   AdGuard v 3.0.x -> i++
+// @license      free
+// ==/UserScript==
+
+;(function() {
+	'use strict'
+
+    for i import {  } from "module";
+    print(): void
+    
+        return 0
